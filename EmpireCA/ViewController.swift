0e25d295e2d5bdc08aef16050b0938e62e03bb9a
Andrius <Userr@macbook-air-andrius.local>
2017-09-01 17:24:02 +0300

    Initial Commit

---
+++ EmpireCA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmpireCA
+//
+//  Created by Andrius on 9/1/17.
+//  Copyright © 2017 Andrius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
